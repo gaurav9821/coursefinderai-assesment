91c11784f7f4cc849ec0804a1315be28481dc9b8
gaurav9821 <gaurav120998chachada@gmail.com>
2025-06-07 19:42:45 +0530

    Commit 2 : Small bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,12 @@ import SubscribeSection from "./components/SubscribeSection";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import WebinarsEvents from "./components/WebinarsEvents";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    window.scrollTo(0, 0); // Force top on load
+  }, []);
   return (
     <div className="app-wrapper">
       <Header />
